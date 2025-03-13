93cadc4f84e3ae31a9541978393a1aa71f2b9467
Tarif Chandan <20030520+TarifChandan@users.noreply.github.com>
2025-03-13 12:56:21 +0600

    Write the Logic to Play a Single Round
    Created three helper functions to define the game rules:
    
    - `rockBeatsScissors`: Determines the winner when either the human or
    computer chooses rock against scissors.
    - `paperBeatsRock`: Determines the winner when either the human or
    computer chooses paper against rock.
    - `scissorsBeatsPaper`: Determines the winner when either the human
    or computer chooses scissors against paper.
    
    Each of these functions evaluates the human and computer choices,
    updating the score accordingly for either the human or computer,
    incrementing their respective score by 1.
    
    Developed a `playRound` function to handle a single round of the game:
    - The function uses `if-else` statements to evaluate both the human's
    and computer's choices:
      - If the human chooses rock and the computer chooses scissors
      (or vice versa), the `rockBeatsScissors` function is executed to
      declare the winner.
      - Similarly, for paper vs. rock and scissors vs. paper,
      the `paperBeatsRock` and `scissorsBeatsPaper` functions are called,
      respectively.

--- script.js
+++ script.js
@@ -1,16 +1,95 @@
+let humanScore = 0;
+let computerScore = 0;
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
+// FUNCTIONS
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
 
   if (randomNumber === 1) {
-    return "Rock";
+    return "rock";
   } else if (randomNumber === 2) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
 function getHumanChoice() {
   let userInput = prompt("Pick your choice: Rock, Paper, or Scissors?");
   return userInput;
+}
+
+function playRound(humanChoice, computerChoice) {
+  let winner;
+  humanChoice = humanChoice.toLowerCase();
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "rock")
+  ) {
+    console.log("Human Choice: " + humanChoice);
+    console.log("Computer Choice: " + computerChoice);
+    winner = rockBeatsScissors(humanChoice, computerChoice);
+    console.log(winner);
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+  } else if (
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "paper")
+  ) {
+    console.log("Human Choice: " + humanChoice);
+    console.log("Computer Choice: " + computerChoice);
+    winner = paperBeatsRock(humanChoice, computerChoice);
+    console.log(winner);
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+  } else if (
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "scissors")
+  ) {
+    console.log("Human Choice: " + humanChoice);
+    console.log("Computer Choice: " + computerChoice);
+    winner = scissorsBeatsRock(humanChoice, computerChoice);
+    console.log(winner);
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+  } else {
+    console.log("It's a draw");
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+  }
+}
+
+function rockBeatsScissors(choiceHuman, choiceComputer) {
+  if (choiceHuman === "rock") {
+    humanScore += 1;
+    return "The Winner is Human";
+  } else if (choiceComputer === "rock") {
+    computerScore += 1;
+    return "The Winner is Computer";
+  }
+}
+
+function paperBeatsRock(choiceHuman, choiceComputer) {
+  if (choiceHuman === "paper") {
+    humanScore += 1;
+    return "The Winner is Human";
+  } else if (choiceComputer === "paper") {
+    computerScore += 1;
+    return "The Winner is Computer";
+  }
+}
+
+function scissorsBeatsRock(choiceHuman, choiceComputer) {
+  if (choiceHuman === "scissors") {
+    humanScore += 1;
+    return "The Winner is Human";
+  } else if (choiceComputer === "scissors") {
+    computerScore += 1;
+    return "The Winner is Computer";
+  }
 }
